48c4e88c8a10fd0e33deaa49758cb00e1d38bced
Nisarg Patel <pate0627@algonquinlive.com>
2021-09-25 02:34:45 -0300

    initial commit

---
+++ counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var counter = 0;
+
+  $("#increment").click(function () {
+    counter++;
+    $("#count").html(counter);
+  });
+
+  $("#decrement").click(function () {
+    counter--;
+    $("#count").html(counter);
+  });
+});
